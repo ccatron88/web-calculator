fa56b2c301afaa5b454eaf3865a8094871cbe993
ccatron88 <christophercatron@outlook.com>
2024-02-25 15:34:32 -0600

    Add index.html, scripts.js, styles.css, and basic functions for simple calculations
    Added empty index.html and styles.css files and made scripts.js files with simple calculation functions for add, subtract, multiply, and divide.

---
+++ scripts.js
@@ -0,0 +1,15 @@
+function add(fNumber, sNumber) {
+    return fNumber + sNumber;
+}
+
+function subtract(fNumber, sNumber) {
+    return fNumber - sNumber;
+}
+
+function multiply(fNumber, sNumber) {
+    return fNumber * sNumber;
+}
+
+function divide(fNumber, sNumber) {
+    return fNumber / sNumber;
+}
