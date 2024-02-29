87ce393b2e64498a260b74db692510b6876dd313
ccatron88 <christophercatron@outlook.com>
2024-02-28 23:32:14 -0600

    Add css styles for calculator and all html elements
    Added html elements including javascript-made elements for number buttons.

--- scripts.js
+++ scripts.js
@@ -1,5 +1,7 @@
 let num1, num2;
 let operator = ''
+let numDivWidth = 300;
+let numDivHeight = 500;
 
 function add(fNumber, sNumber) {
     return fNumber + sNumber;
@@ -28,3 +30,18 @@ const operate = (num1, num2, operator) => {
         return divide(num1, num2);
     }
 }
+
+const loadNumerals = () => {
+    const numeralDiv = document.querySelector('#numerals')
+    
+    for (let i = 0; i < 10; i++) {
+        const numKey = document.createElement('button');
+        numKey.classList.add('number-key')
+        numKey.style.width = (numDivWidth / 10);
+        numKey.style.height = (numDivHeight / 10);
+        numKey.innerText = i;
+        numeralDiv.appendChild(numKey);
+    }
+}
+
+document.addEventListener('DOMContentLoaded', loadNumerals())
