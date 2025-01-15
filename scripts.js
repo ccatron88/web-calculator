16fcebe80273a41505715f5c5b8d0a271c841ba6
ccatron88 <christophercatron@outlook.com>
2025-01-15 15:24:14 -0600

    Committing for use across PCs

--- scripts.js
+++ scripts.js
@@ -1,9 +1,14 @@
 let num1, num2;
 let operator = ''
+let displayValue = 0;
+let keyPressCount = 0;
 let numDivWidth = 300;
 let numDivHeight = 500;
 const numKeyWidth = numDivWidth * .13;
 const numKeyHeight = numDivHeight * .13;
+const displayEl = document.querySelector('#display-result');
+
+displayEl.innerText = displayValue;
 
 function add(fNumber, sNumber) {
     return fNumber + sNumber;
@@ -22,15 +27,38 @@ function divide(fNumber, sNumber) {
 }
 
 const operate = (num1, num2, operator) => {
+    console.log('fired');
     if (operator === '+') {
+        keyPressCount = 0;
         return add(num1, num2);
     } else if (operator === '-') {
+        keyPressCount = 0;
         return subtract(num1, num2);
     } else if (operator === '*') {
+        keyPressCount = 0;
         return multiply(num1, num2);
     } else if (operator === '/') {
+        keyPressCount = 0;
         return divide(num1, num2);
     }
+}
+
+const updateDisplay = (text) => {
+    const regNum = /\d/.exec(text);
+
+    if (regNum && keyPressCount == 0) {
+        console.log('fired0');
+        displayEl.innerText = regNum;
+        num1 = text;
+    } else if ((regNum && keyPressCount == 1) || (regNum && keyPressCount == 2)) {
+        console.log('fired12');
+        displayEl.innerText = regNum;
+        num2 = text;
+    } else {
+        console.log('firedOp');
+        operator = text;
+    }
+    keyPressCount++;
 }
 
 const loadNumerals = () => {
@@ -38,7 +66,7 @@ const loadNumerals = () => {
     const actionsEl = `
         <div id="actions-container">
             <div id="clr"><button class="action-key" style="height: ${numKeyHeight}px; width: ${numKeyWidth}px;">C</button></div>
-            <div id="equal"><button class="action-key" style="height: ${numKeyHeight}px; width: ${numKeyWidth}px;">=</button></div>
+            <div id="equal"><button id="equals-btn" class="action-key" style="height: ${numKeyHeight}px; width: ${numKeyWidth}px;">=</button></div>
         </div>
     `
     
@@ -49,11 +77,33 @@ const loadNumerals = () => {
         numKey.style.height = `${numKeyHeight}px`;
         numKey.innerText = i;
         numeralDiv.appendChild(numKey);
+
+        numKey.addEventListener('click', (event) => {
+            updateDisplay(event.currentTarget.innerText)
+        });
     }
     numeralDiv.insertAdjacentHTML('beforeend', actionsEl);
-
-
-
 }
 
-document.addEventListener('DOMContentLoaded', loadNumerals())
+// document.addEventListener('DOMContentLoaded', () => {
+//     loadNumerals();
+//     const equalsEl = document.querySelector('#equal')
+    
+//     equalsEl.addEventListener('click', () => {
+//         operate(num1, num2, operator);
+//     })
+// });
+loadNumerals();
+if (document.readyState !== "loading") {
+    const equalsEl = document.querySelector("#equals-btn");
+    const operatorEl = document.querySelectorAll('.operator');
+    const operatorArr = [...operatorEl]
+
+    equalsEl.addEventListener('click', () => {
+        displayEl.innerText = operate(num1, num2, operator);
+    })
+
+    operatorArr.forEach(item => {
+        item.addEventListener('click', () => { updateDisplay(item.innerText) })
+    })
+}
